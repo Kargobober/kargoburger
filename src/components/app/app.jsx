983f88ce5b0dddbdc488bfc346c336353330c26a
Kargobober <kargobober@yandex.ru>
2023-10-12 19:39:15 +0300

    beginning
    before rename app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,15 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
-    </div>
+
   );
 }
 
