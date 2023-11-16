2f10714cd2b441eaf50fb52cc3472db3db5b3eac
Kargobober <kargobober@yandex.ru>
2023-11-16 23:04:01 +0300

    feat: responsive and works header links

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -5,17 +5,12 @@ import HeaderLink from './HeaderLink/HeaderLink';
 
 function AppHeader() {
   const headerLinks = [
-    { name: 'Конструктор', img: BurgerIcon },
-    { name: 'Лента заказов', img: ListIcon },
-    { name: 'Личный кабинет', img: ProfileIcon },
+    { name: 'Конструктор', img: BurgerIcon, to: '/' },
+    { name: 'Лента заказов', img: ListIcon, to: '/orders-global' },
+    { name: 'Личный кабинет', img: ProfileIcon, to: '/profile' },
   ] //кавычки не ставим в значении поля иконок, иначе реакт не поймёт
-  const [currentSection, setCurrentSection] = React.useState(headerLinks[0].name);
 
-  const toggleSection = (sectionName) => (evt) => {
-    setCurrentSection(sectionName);
-  }
 
-  const isActive = (index) => headerLinks[index].name === currentSection ? true : false;
 
   return (
     <header className={`${styles.header} pt-4 pb-4`}>
@@ -26,16 +21,14 @@ function AppHeader() {
             <HeaderLink
               sectionName={headerLinks[0].name}
               Icon={headerLinks[0].img}
-              callback={toggleSection(headerLinks[0].name)}
-              isActive={isActive(0)}
+              to={headerLinks[0].to}
             />
           </li>
           <li>
             <HeaderLink
               sectionName={headerLinks[1].name}
               Icon={headerLinks[1].img}
-              callback={toggleSection(headerLinks[1].name)}
-              isActive={isActive(1)}
+              to={headerLinks[1].to}
             />
           </li>
         </ul>
@@ -45,8 +38,7 @@ function AppHeader() {
         <HeaderLink
           sectionName={headerLinks[2].name}
           Icon={headerLinks[2].img}
-          callback={toggleSection(headerLinks[2].name)}
-          isActive={isActive(2)}
+          to={headerLinks[2].to}
           customStyle={
             { justifySelf: 'end', } //объект стилей
           }
