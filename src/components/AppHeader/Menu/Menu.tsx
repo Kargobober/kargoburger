1c062d2b89e37062eb06f83862ed3a29919ef9c1
Kargobober <kargobober@yandex.ru>
2024-03-01 13:53:37 +0300

    feat: add dropdown menu for personal area

--- src/components/AppHeader/Menu/Menu.tsx
+++ src/components/AppHeader/Menu/Menu.tsx
@@ -1,9 +1,11 @@
-import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { FC, ReactNode, useEffect } from 'react';
+import { ArrowDownIcon, CloseIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { FC, useEffect, useRef, useState } from 'react';
 import styles from './Menu.module.css';
 import ModalOverlay from '../../ModalOverlay/ModalOverlay';
 import HeaderLink from '../HeaderLink/HeaderLink';
 import { TIconProps } from '@ya.praktikum/react-developer-burger-ui-components/dist/ui/icons/utils';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Collapse } from 'react-collapse';
 
 type TProps ={
   data: Array<{
@@ -11,10 +13,30 @@ type TProps ={
     img: FC<TIconProps>,
     to: string,
   }>;
+  /**
+   * обработчик переключения видимости поповера (toggle-режим)
+   */
   handler?: () => void;
 };
 
 const Menu: FC<TProps> = ({data, handler}) => {
+  const location = useLocation();
+
+  const [isProfileButtonActive, setIsProfileButtonActive] = useState(false);
+  useEffect(() => {
+    // класс active добавляется встроенным функционалом NavLink из react-router-dom
+    if (location.pathname.includes('profile')) {
+      setIsProfileButtonActive(true);
+      setIsMenuWindowOpen(true);
+    } else {
+      setIsProfileButtonActive(false);
+    }
+  }, [location]);
+
+  const [isMenuWindowOpen, setIsMenuWindowOpen] = useState(false);
+  const onProfileButtonClick = () => {
+    setIsMenuWindowOpen(prev => !prev);
+  }
   useEffect(() => {
     if (handler) {
       const handleEsc = (evt: KeyboardEvent) => {
@@ -39,13 +61,69 @@ const Menu: FC<TProps> = ({data, handler}) => {
         </div>
 
         <nav>
-          <ul className='listGlobal text_color_primary'>
+          <ul className={`listGlobal text_color_primary ${styles.menuList}`}>
             <li>
-              <HeaderLink
-                sectionName={data[2].name}
-                Icon={data[2].img}
-                to={data[2].to}
-              />
+              <button
+                className={`buttonGlobal text_type_main-default ${styles.buttonProfile}` + ' ' + (isProfileButtonActive ? 'text_color_primary' : 'text_color_inactive')}
+                onClick={onProfileButtonClick}
+              >
+                <div className={styles.personIcoWrapper}>
+                  <ProfileIcon type={isProfileButtonActive ? 'primary' : 'secondary'} />
+                </div>
+                Личный кабинет
+                <div className={styles.arrowWrapper}>
+                  <ArrowDownIcon type={isProfileButtonActive ? 'primary' : 'secondary'} />
+                </div>
+              </button>
+              <Collapse
+                isOpened={isMenuWindowOpen}
+              >
+                <ul className='listGlobal text_color_primary'>
+                  <li className={styles.profileLinks}>
+                    <NavLink
+                    // черта после profile влияет на правильное распознование активности ссылки
+                      to='/profile/'
+                      className={({isActive}) =>
+                        [
+                          'linkGlobal',
+                          isActive ? 'text_color_primary' : 'text_color_inactive',
+                          'text_type_main-default'
+                        ].join(' ')
+                      }
+                    >
+                      Профиль
+                    </NavLink>
+                  </li>
+                  <li className={styles.profileLinks}>
+                    <NavLink
+                      to='/profile/orders'
+                      className={({isActive}) =>
+                        [
+                          'linkGlobal',
+                          isActive ? 'text_color_primary' : 'text_color_inactive',
+                          'text_type_main-default'
+                        ].join(' ')
+                      }
+                    >
+                      История заказов
+                    </NavLink>
+                  </li>
+                  <li className={styles.profileLinks}>
+                    <NavLink
+                      to='/profile/logout'
+                      className={({isActive}) =>
+                        [
+                          'linkGlobal',
+                          isActive ? 'text_color_primary' : 'text_color_inactive',
+                          'text_type_main-default'
+                        ].join(' ')
+                      }
+                    >
+                      Выход
+                    </NavLink>
+                  </li>
+                </ul>
+              </Collapse>
             </li>
             <li>
               <HeaderLink
