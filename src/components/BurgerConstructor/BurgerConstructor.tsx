dcf96bf92f520cb79af0eab6f6d87365b009da90
Kargobober <kargobober@yandex.ru>
2026-01-04 11:43:13 +0300

    fix(orders): order is not sending on desktop

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,25 +1,44 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
-import styles from './BurgerConstructor.module.css';
+import styles from "./BurgerConstructor.module.css";
 
-import { handleError } from '../../utils/utils';
-import { useDispatch, useSelector } from '../../services/hooks';
+import { handleError } from "../../utils/utils";
+import { useDispatch, useSelector } from "../../services/hooks";
 
-import { BurgerIcon, Button, ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
-import Price from '../Price/Price';
-import Item from './Item/Item';
-import OrderDetails from '../OrderDetails/OrderDetails';
-import Modal from '../Modal/Modal';
-import { getSelectedBun, getSelectedProducts, getTotalPrice } from '../../services/selectors/burgerConstructorSelector';
-import { getOrderDetailsNeeding, getOrderError, getOrderSuccess } from '../../services/selectors/orderDetailsSelector';
-import { resetOrderNumber, setNeedingDetails } from '../../services/slices/orderDetailsSlice';
-import burgerIconSvg from '../../images/burger.svg';
-import { useDrop } from 'react-dnd';
-import { addItem, resetConstructor } from '../../services/slices/burgerConstructorSlice';
-import { useNavigate } from 'react-router';
-import { getUserFromState } from '../../services/selectors/authSelector';
-import { TIngredientCounted } from '../../utils/types';
-import useWindowSize from '../../utils/hooks/useWindowSize';
+import {
+  BurgerIcon,
+  Button,
+  ConstructorElement,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import Price from "../Price/Price";
+import Item from "./Item/Item";
+import OrderDetails from "../OrderDetails/OrderDetails";
+import Modal from "../Modal/Modal";
+import {
+  getSelectedBun,
+  getSelectedProducts,
+  getTotalPrice,
+} from "../../services/selectors/burgerConstructorSelector";
+import {
+  getOrderDetailsNeeding,
+  getOrderError,
+  getOrderSuccess,
+} from "../../services/selectors/orderDetailsSelector";
+import {
+  resetOrderNumber,
+  setNeedingDetails,
+} from "../../services/slices/orderDetailsSlice";
+import burgerIconSvg from "../../images/burger.svg";
+import { useDrop } from "react-dnd";
+import {
+  addItem,
+  resetConstructor,
+} from "../../services/slices/burgerConstructorSlice";
+import { postOrder } from "../../services/middlewares/orderDetailsQueries";
+import { useNavigate } from "react-router";
+import { getUserFromState } from "../../services/selectors/authSelector";
+import { TIngredientCounted } from "../../utils/types";
+import useWindowSize from "../../utils/hooks/useWindowSize";
 
 function BurgerConstructor() {
   const dispatch = useDispatch();
@@ -27,8 +46,8 @@ function BurgerConstructor() {
   const windowSize = useWindowSize();
 
   const paddingForModal = {
-    pt: windowSize.width > 1049 ? '15' : 'неважно', // для 'pt-...'
-    pb: windowSize.width > 1049 ? '30' : 'в-другом-месте-код',
+    pt: windowSize.width > 1049 ? "15" : "неважно", // для 'pt-...'
+    pb: windowSize.width > 1049 ? "30" : "в-другом-месте-код",
     // При меньших размерах экрана (до 1050) используется код order details, описанный внутри: BurgerIngridients > Bottom > Cart.tsx.
     // Относительно DOM модалка располагается в том же реактовском портале.
   };
@@ -43,13 +62,11 @@ function BurgerConstructor() {
   const user = useSelector(getUserFromState);
 
   useEffect(() => {
-    error && handleError('Ошибка при создании заказа: ', error);
+    error && handleError("Ошибка при создании заказа: ", error);
   }, [error]);
 
   const modal = (
-    <Modal
-      onClose={onClose}
-    >
+    <Modal onClose={onClose}>
       <OrderDetails />
     </Modal>
   );
@@ -59,20 +76,21 @@ function BurgerConstructor() {
   const totalPrice = useSelector(getTotalPrice);
 
   useEffect(() => {
-    if(isOrderSucces) dispatch(resetConstructor());
+    if (isOrderSucces) dispatch(resetConstructor());
   }, [isOrderSucces]);
 
   function handleOrder() {
     const assembledBurger: string[] = []; // массив айдишек ингредиентов
 
     if (selectedBun) assembledBurger.push(selectedBun._id); // но кнопка заказать неактивна, пока нет булочки, так что это ЕЩЁ одна проверка
-    assembledBurger.push(...selectedProducts.map(el => el._id));
+    assembledBurger.push(...selectedProducts.map((el) => el._id));
     if (selectedBun) assembledBurger.push(selectedBun._id);
 
-    if (user === null ? false : (user.name && user.email ? true : false)) {
+    if (user === null ? false : user.name && user.email ? true : false) {
       dispatch(setNeedingDetails(true));
+      dispatch(postOrder({ ingredients: assembledBurger }));
     } else {
-      navigate('/login');
+      navigate("/login");
     }
   }
 
@@ -81,41 +99,52 @@ function BurgerConstructor() {
     dispatch(resetOrderNumber());
   }
 
-
   // дропается ингредиент с новым полем - qty, а при диспатче добавляем ещё и уник. айдишник
-  const [{ isHover }, dropRef] = useDrop<{card: TIngredientCounted}, unknown, {isHover: boolean}>({
-    accept: 'ingredient',
+  const [{ isHover }, dropRef] = useDrop<
+    { card: TIngredientCounted },
+    unknown,
+    { isHover: boolean }
+  >({
+    accept: "ingredient",
     // деструктуризация объекта
     drop: ({ card }) => {
       dispatch(addItem(card));
     },
-    collect: monitor => ({
+    collect: (monitor) => ({
       isHover: monitor.isOver(),
     }),
   });
 
-
-
   return (
     <>
       {needDetails && isOrderSucces !== false && modal}
-      <section className={`${styles.sectionCommon} pt-25 pb-5`} ref={sectionElem}>
-
-        <section ref={dropRef} className={isHover ? `${styles.sectionConstructor} ${styles.dropTarget}` : styles.sectionConstructor}>
-
-          {!selectedBun && selectedProducts.length === 0 &&
+      <section
+        className={`${styles.sectionCommon} pt-25 pb-5`}
+        ref={sectionElem}
+      >
+        <section
+          ref={dropRef}
+          className={
+            isHover
+              ? `${styles.sectionConstructor} ${styles.dropTarget}`
+              : styles.sectionConstructor
+          }
+        >
+          {!selectedBun && selectedProducts.length === 0 && (
             <div className={styles.stub}>
-              <BurgerIcon type='secondary' />
-              <p className={`text text_type_main-default text_color_inactive ${styles.stubText}`}>
+              <BurgerIcon type="secondary" />
+              <p
+                className={`text text_type_main-default text_color_inactive ${styles.stubText}`}
+              >
                 {` Добавьте ингредиенты двойным кликом \n или перетаскиванием `}
               </p>
-              <BurgerIcon type='secondary' />
+              <BurgerIcon type="secondary" />
             </div>
-          }
+          )}
 
           {/* верхняя булка */}
           {selectedBun && (
-            <div className='pl-8 pr-4'>
+            <div className="pl-8 pr-4">
               <ConstructorElement
                 text={`${selectedBun.name} (верх)`}
                 thumbnail={selectedBun.image}
@@ -127,9 +156,9 @@ function BurgerConstructor() {
             </div>
           )}
           {selectedProducts.length > 0 && !selectedBun && (
-            <div className='pl-8 pr-4'>
+            <div className="pl-8 pr-4">
               <ConstructorElement
-                text='Добавьте булку'
+                text="Добавьте булку"
                 thumbnail={burgerIconSvg}
                 price={0}
                 type="top"
@@ -140,19 +169,19 @@ function BurgerConstructor() {
           )}
 
           {/* внутренности бургера */}
-          <ul className={`${styles.listOfFillings} custom-scroll`} ref={fillingsElem}>
-            {selectedProducts.length > 0
-              && selectedProducts.map((el, index) => <Item
-                key={el.extraId}
-                ingredient={el}
-                index={index}
-              />)
-            }
+          <ul
+            className={`${styles.listOfFillings} custom-scroll`}
+            ref={fillingsElem}
+          >
+            {selectedProducts.length > 0 &&
+              selectedProducts.map((el, index) => (
+                <Item key={el.extraId} ingredient={el} index={index} />
+              ))}
           </ul>
 
           {/* нижняя булочка */}
           {selectedBun && (
-            <div className='pl-8 pr-4'>
+            <div className="pl-8 pr-4">
               <ConstructorElement
                 text={`${selectedBun.name} (низ)`}
                 thumbnail={selectedBun.image}
@@ -164,9 +193,9 @@ function BurgerConstructor() {
             </div>
           )}
           {selectedProducts.length > 0 && !selectedBun && (
-            <div className='pl-8 pr-4'>
+            <div className="pl-8 pr-4">
               <ConstructorElement
-                text='Хлеб всему квазар'
+                text="Хлеб всему квазар"
                 thumbnail={burgerIconSvg}
                 price={0}
                 type="bottom"
@@ -178,19 +207,21 @@ function BurgerConstructor() {
         </section>
 
         <section className={styles.sectionPrice}>
-          <Price value={totalPrice} digitsSize='medium' svgSize='32' />
-          <Button htmlType="button" type="primary" size="medium"
+          <Price value={totalPrice} digitsSize="medium" svgSize="32" />
+          <Button
+            htmlType="button"
+            type="primary"
+            size="medium"
             onClick={handleOrder}
             disabled={!selectedBun}
-            extraClass={selectedBun ? 'button_decor_shadow' : ''}
+            extraClass={selectedBun ? "button_decor_shadow" : ""}
           >
             Оформить заказ
           </Button>
         </section>
-
       </section>
     </>
-  )
+  );
 }
 
 export default BurgerConstructor;
